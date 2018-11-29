aa6c5e81ad91f671e2b4cd60261f163de4a6cce6
dirakx1 <35827025+dirakx1@users.noreply.github.com>
2018-11-29 13:17:05 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-variable "gcp_project_id" {}
-variable "project_id" {}
-variable "gcp_credentials" {}
-variable "config_name" {}
 variable "gcp_zone" {}
-variable "zone" {}
-variable "instance_id" {}
-variable "uptime_check_id" {}
-variable "cluster_name" {}
-variable "image_type" {}
-variable "machine_type" {}
-variable "node_version" {}
-variable "min_master_version" {}
+
+gcp_zone = "test"
