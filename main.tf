5f86aef72ecf535af00993292eb9b2c5344eda38
dirakx1 <35827025+dirakx1@users.noreply.github.com>
2018-11-30 15:30:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
-variable "gcp_zone" {
-  default = "test"
+provider "google" {
+  credentials = "${var.gcp_credentials}"
+  project     = "nc-te-teststackdrive001-plygrd"
+  region      = "us-central1"
+}
+
+resource "null_resource" "uptimeupdate" {
+
+  provisioner "local-exec" {
+    command = "echo"
+
+    environment {
+      GCLOUD_PROJECT                 = "nc-te-teststackdrive001-plygrd"
+      GOOGLE_APPLICATION_CREDENTIALS = "${var.gcp_credentials}"
+    }
+  }
 }
 
